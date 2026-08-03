4e985e93afc11b230b13273458fa7d256ab76d5d
balajiinteriorhyd-ui <balaji.interiorhyd@gmail.com>
2026-08-04 00:46:24 +0530

    Added mobile hamburger menu

--- script.js
+++ script.js
@@ -77,3 +77,11 @@ boxes.forEach((box)=>{
 lightbox.addEventListener("click",()=>{
   lightbox.style.display="none";
 });
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+
+if(menuBtn){
+menuBtn.addEventListener("click",()=>{
+nav.classList.toggle("active");
+});
+}
